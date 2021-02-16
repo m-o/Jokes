4eafc4e0d96b48336806f001edc594e484b8773a
Marek <marek.ovecka@gmail.com>
2021-02-16 23:18:22 +0100

    refactored main activity

--- app/src/main/java/name/ovecka/jokes/MainActivity.kt
+++ app/src/main/java/name/ovecka/jokes/MainActivity.kt
@@ -2,18 +2,15 @@ package name.ovecka.jokes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import name.ovecka.jokes.databinding.MainActivityBinding
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
-    private val topLevelScreens = setOf(R.id.mainFragment,R.id.listFragment)
+
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +18,6 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
         binding.bottomNavigation.setupWithNavController(navController)
-        setupActionBarWithNavController(navController, AppBarConfiguration(topLevelScreens))
     }
 }
